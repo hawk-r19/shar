796ffd5e04d53bddf9f09f6031d28fbf21a26b62
Rasheek Huq <rhawk1509@gmail.com>
2023-06-08 15:20:58 -0500

    nav basic layout and styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,13 +6,13 @@ export default function Nav() {
     return (
         <React.Fragment>
             <nav>
-                <img src='../imgs/ex_logo.png' alt='logo' />
+                <img src={require('../imgs/ex_logo.png')} alt='logo' />
                 <div className='nav-right'>
                     <div className='nav-links'>
                         <Link className='nav-link nav-home' to='/'>Home</Link>
                         <Link className='nav-link nav-schedule' to='/'>Schedule a Lesson!</Link>
                         <Link className='nav-link nav-tips' to='/'>Tips</Link>
-                        <Link className='nav-link nav-about' to='/'>About</Link>
+                        {/* <Link className='nav-link nav-about' to='/'>About</Link> */}
                         <Link className='nav-link nav-contact' to='/'>Contact</Link>
                     </div>
                 </div>

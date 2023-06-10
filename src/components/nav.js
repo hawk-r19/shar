30c792792bfc018ce4d8666e93d05c672243aa40
Rasheek Huq <rhawk1509@gmail.com>
2023-06-10 18:02:20 -0500

    nav layout/styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,11 +6,15 @@ import {ReactComponent as Logo} from '../imgs/shar_logo.svg'
 export default function Nav() {
     return (
         <nav>
-            <Link className='nav-link nav-schedule' to='/'>Schedule a Lesson!</Link>
-            <Link className='nav-link nav-tips' to='/'>Tennis Tips</Link>
+            <div className='nav-left'>
+                <div className='nav-link nav-about'>About</div>
+                <Link className='nav-link nav-schedule' to='/'>Schedule a Lesson!</Link>
+            </div>
             <Link className='nav-link nav-home' to='/'><Logo className='logo' /></Link>
-            <div className='nav-link nav-about'>About</div>
-            <Link className='nav-link nav-contact' to='contact'>Contact</Link>
+            <div className='nav-right'>
+                <Link className='nav-link nav-tips' to='/'>Tennis Tips</Link>
+                <Link className='nav-link nav-contact' to='contact'>Contact</Link>
+            </div>
         </nav>
     )
 }

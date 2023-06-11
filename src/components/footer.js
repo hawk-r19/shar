275e417ed0b577ccdd268fdf31bd308a3e41a2d2
Rasheek Huq <rhawk1509@gmail.com>
2023-06-11 11:49:59 -0500

    home styling, hide scrollbar

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import '../styles/footer.css'
 export default function Footer() {
     return (
         <div className='footer'>
-            <a className='logo-credit' target='_blank' href="https://www.vecteezy.com/free-vector/tennis-logo">Tennis Logo Vectors by Vecteezy</a>
+            <a className='logo-credit' target='_blank' rel='noreferrer' href="https://www.vecteezy.com/free-vector/tennis-logo">Tennis Logo Vectors by Vecteezy</a>
         </div>
     )
 }

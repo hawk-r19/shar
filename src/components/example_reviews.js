a76fa440da97a5676786ab4faba0d76e0df2bb73
Rasheek Huq <rhawk1509@gmail.com>
2023-06-11 00:57:10 -0500

    reviews styling, footer link target

---
+++ src/components/example_reviews.js
@@ -0,0 +1,16 @@
+//example reviews in case firebase isn't available
+
+import thiem from '../imgs/thiem.png'
+
+const example1 = {
+    name: "Dominic Thiem",
+    img_src: thiem,
+    review: `"Nice experience. He has been analysing my play and identified aspects of my game to improve upon. He sets up routines to improve on those areas."`
+}
+const example2 = {
+    name: "Roger",
+    review: `"Shar has a wonderful mix of high standards while making it fun. My kids and I are enjoying our lessons with him immensely!"`
+}
+var ex_reviews = [example1, example2];
+
+export {ex_reviews};
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,25 +2,19 @@ import React from 'react'
 import '../styles/home.css'
 import {Link} from 'react-router-dom'
 import {ReactComponent as Hero} from '../imgs/sharbackaction.svg'
+import {ex_reviews} from '../components/example_reviews.js';
 
-import thiem from '../imgs/thiem.png'
 const introText = "I'm Shahriyar (you can call me Shar), a tennis coach and high school teacher based in South Austin. I offer professional quality private lessons for teens and adults of any skill level, scheduled at your convenience! More...";
 const scheduleText = "To request a lesson, just click here, fill in your information, select an available time slot, and submit! I'll get back to you as soon as I can to confirm your appointment.";
 const aboutText = "Some extra stuff about me, my experience, history, maybe skill level, etc Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit gravida rutrum quisque non. More...";
 
-//need to set up retrieving reviews from database
-const example1 = {
-    name: "Dominic Thiem",
-    img_src: thiem,
-    review: `"Nice experience. He has been analysing my play and identified aspects of my game to improve upon. He sets up routines to improve on those areas."`
-}
-const example2 = {
-    name: "Roger",
-    review: `"Shar has a wonderful mix of high standards while making it fun. My kids and I are enjoying our lessons with him immensely!"`
-}
-var reviews = [example1, example2];
+export default function Home(props) {
+    var reviews;
+    if('firebase' in props) reviews = [/* retrieve from firebase */]
+    else {
+        reviews = ex_reviews;
+    }
 
-export default function Home() {
     return (
         <div className="page home-page">
             <div className='hero-div'>

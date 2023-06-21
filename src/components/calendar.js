1dabfd29dd47750c7f621a13c9209ef7fad61526
Rasheek Huq <rhawk1509@gmail.com>
2023-06-21 17:46:12 -0500

    schedule page form structure

---
+++ src/components/calendar.js
@@ -0,0 +1,7 @@
+import '../styles/calendar.css'
+
+export default function Calendar(props) {
+    return (
+        <div className='calendar'>calendar div</div>
+    )
+}
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,24 +1,165 @@
 import '../styles/schedule.css'
 import dayjs from 'dayjs'
 import {useState} from 'react'
+import Calendar from '../components/calendar.js'
 
 export default function SchedulePage() {
     const [submitted, setSubmitted] = useState(false);
     const [lastBooked, setLastBooked] = useState({});
+    const [info, setInfo] = useState({
+        forMe: true,
+        name: '',
+        studentName: '',
+        age: '',
+        skill: '',
+        firstTime: true,
+        focus: '',
+        notes: '',
+        date: '',
+        length: '',
+        time: '',
+    });
+
+    const onSubmitForm = e => {
+        //handling submit to firebase, updating state
+    }
+
+    const handleChange = e => {
+        setInfo({
+            ...info,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    /* all info needed:
+        Scheduling for yourself or for someone else?
+        your name/student's name - set this to same if above is checked/unchecked
+        your/student's age
+        skill level (optional)
+        is this your first session with me?
+        anything specific you would like to focus on/do?
+        Message/other info or concerns:
+        calendar
+        payment info
+    */
 
     return (
         <div className='page schedule-page'>
-            
+            {submitted ? <PostSubmitInfo lastBooked={lastBooked} /> : 
+                <div className='schedule-form-div'>
+                    <div className='initial-info-div'>
+                        initial info div
+                    </div>
+                    <div className='form-div'>
+                        <form onSubmit={onSubmitForm}>
+                            <div className='for-me-div'>
+                                <div className='for-me-head'>I'm booking... </div>
+                                <div className='for-me-container'>
+                                    <label for='forMe'>For Me:</label>
+                                    <input type='radio' id='for-me' name='forMe' 
+                                        value='forMe' checked={info.forMe} onChange={e => {
+                                            setInfo({...info, forMe: e.target.value})
+                                        }}/>
+                                </div>
+                                <div className='for-else-container'>
+                                    <label for='forElse'>For Someone Else:</label>
+                                    <input type='radio' id='for-else' name='forMe' 
+                                        value='forElse' checked={!info.forMe} onChange={e => {
+                                            setInfo({...info, forMe: e.target.value})
+                                        }}/>
+                                </div>
+                            </div>
+                            <div className='form-name-div'>
+                                <div className='name-div'>
+                                    <label for='name'>Your name:</label>
+                                    <input type='text' name='name' id='name' value={info.name} onChange={handleChange}/>
+                                </div>
+                                {info.forMe ? null : 
+                                    <div className='student-name-div'>
+                                        <label for='studentName'>Student's Name:</label>
+                                        <input type='text' name='studentName' id='student-name' 
+                                            value={info.studentName} onChange={handleChange}/>
+                                    </div>
+                                }
+                            </div>
+                            <div className='age-div'>
+                                <label for='age'>{info.forMe ? 'Age:' : "Student's Age:"}</label>
+                                <input type='number' name='age' id='age' value={info.age} onChange={handleChange}/>
+                            </div>
+                            <div className='skill-div'>
+                                <label for='skill'>{info.forMe ? 'Skill Level:' : "Student's Skill Level:"}</label>
+                                <input type='text' name='skill' id='skill' value={info.skill} onChange={handleChange}/>
+                            </div>
+                            <div className='first-time-div'>
+                                <label for='firstTime'>Is this your first time working with me?</label>
+                                <input type='checkbox' name='firstTime' id='first-time' 
+                                    checked={info.firstTime} onChange={e => {
+                                        setInfo({
+                                            ...info,
+                                            firstTime: e.target.checked,
+                                        });
+                                    }}/>
+                            </div>
+                            <div className='focus-div'>
+                                <label for='focus'>What would you like to work on in this session?</label>
+                                <input type='text' name='focus' id='focus' value={info.focus} onChange={handleChange}/>
+                            </div>
+                            {!info.firstTime ? <></> :
+                                <div className='availability-div'>
+                                    <div className='availability-info-short'>some availability info</div>
+                                    <label for='availabilty'>What days/times would you like your first lesson to be?</label>
+                                    <input type='text' name='availability' id='availability' onChange={handleChange}/>
+                                </div>
+                            }
+                            <div className='notes-div'>
+                                <label for='notes'>Other Concerns/Requirements:</label>
+                                <textarea name='notes' id='notes' value={info.notes} onChange={handleChange}/>
+                            </div>
+                            {info.firstTime ? <></> : 
+                                <div className='schedule-time-div'>
+                                    <Calendar />
+                                    <div className='schedule-time-inputs'>
+                                        <div className='date-div'>
+                                            <label for='date'>Date:</label>
+                                            <input type='text' name='date' id='date' value={info.date} onChange={handleChange}/>
+                                        </div>
+                                        <div className='length-div'>
+                                            <label for='length'>Session Length:</label>
+                                            <select name='length' onChange={handleChange}>
+                                                <option value='60'>60 Minutes</option>
+                                                <option value='75'>75 Minutes</option>
+                                                <option value='90'>90 Minutes</option>
+                                            </select>
+                                        </div>
+                                        <div className='time-div'>
+                                            <label for='time'>Time:</label>
+                                            <input type='time' name='time' id='time' value={info.time} onChange={handleChange}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                        </form>
+                    </div>
+                </div>
+            }
+            <PaymentInfo />
         </div>
     )
 }
 
-function Payment(props) {
+function PaymentInfo(props) {
     return (
         <div className='pay-div'>Payment Info Coming Soon</div>
     )
 }
 
+function PostSubmitInfo(props) {
+    return (
+        <div className='post-submit-info-div'>
+            {props.lastBooked}
+        </div>
+    )
+}
 
 /* import '../styles/schedule.css'
 import dayjs from 'dayjs'

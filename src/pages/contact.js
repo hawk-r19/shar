d5b163ea6e46c182911edaeb8ee4f551ca6e2d26
Rasheek Huq <rhawk1509@gmail.com>
2023-07-21 13:55:58 -0500

    contact page styling, post contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,13 +6,13 @@ import _ from 'lodash'
 
 export default function Contact({mobile}) {
     const [info, setInfo] = useState({
-        client_email: '',
         client_name: '',
+        client_email: '',
         message: ''
     });
     const [lastEmail, setLastEmail] = useState({
-        client_email: '',
         client_name: '',
+        client_email: '',
         message: ''
     });
     const [submitted, setSubmitted] = useState(false);
@@ -23,12 +23,78 @@ export default function Contact({mobile}) {
 
     const submitForm = e => {
         e.preventDefault();
-        const tmplt_params = {...info};
-        setLastEmail({...info});
-        console.log(tmplt_params);
+        const updateLastEmail = () => {setLastEmail(...info)};
+        if(_.isEqual(info, lastEmail)) {alert('An appointment with this exact information was already sent recently.');}
+        else {
+            /* emailjs.send('default_service', 'tmplt_contact', tmplt_params, EMAILJS_KEY).then(response => {
+                console.log('EMAILJS SUCCESS', response.status, response.text);
+                updateLastEmail();
+                setSubmitted(true);
+            }, error => {
+                console.log('EMAILJS FAILED', error);
+                alert('Sending email failed, try clicking submit again.');
+            }); */
+            const tmplt_params = {...info};
+            setLastEmail({...info});
+            setSubmitted(true);
+        }
+    }
+
+    const handleChange = e => {
+        setInfo({
+            ...info,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const backToContact = () => {
+        setInfo({
+            client_name: '',
+            client_email: '',
+            message: ''
+        });
+        setSubmitted(false);
     }
 
     return (
-        <div className={'page contact-page' + (mobile ? ' mobile' : '')}>Contact Page Coming Soon!</div>
+        <div className={'page contact-page' + (mobile ? ' mobile' : '')}>
+            {submitted ? 
+                <div className='post-contact-div'>
+                    <div className='post-header'>Message Sent</div>
+                    <div className='again-button-div'>
+                        <div className='again-button' onClick={backToContact}>
+                            Send Another Message
+                        </div>
+                    </div>
+                </div> :
+                <form className='contact-form-div' onSubmit={submitForm}>
+                    <div className='contact-header'>Contact Me</div>
+                    <div className='contact-text-input-div contact-name-div'>Name
+                        <span className='input-wrap name-wrap'>
+                            <span className='input-width name-width' aria-hidden="true">{info.client_name}</span>
+                            <input type='text' id='name' name='client_name' required autoComplete='on'
+                                value={info.client_name} onChange={handleChange}/>
+                        </span>
+                    </div>
+                    <div className='contact-text-input-div contact-email-div'>Email
+                        <span className='input-wrap email-wrap'>
+                            <span className='input-width email-width' aria-hidden="true">{info.client_email}</span>
+                            <input type='email' id='email' name='client_email' required autoComplete='on'
+                                value={info.client_email} onChange={handleChange}/>
+                        </span>
+                    </div>
+                    <div className='message-div'>Message
+                        <span className='message-wrap'>
+                            <p className='message-height' aria-hidden='true'>
+                                {(info.message === '') ? 'a' : info.message}</p>
+                            <textarea name='message' id='message' value={info.message} onChange={handleChange}/>
+                        </span>
+                    </div>
+                    <div className='submit-div'>
+                        <button type='submit' className='submit-button'>Submit</button>
+                    </div>
+                </form>
+            }
+        </div>
     )
 }
